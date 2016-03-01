c911dd07114ecbb565c1065fe0ff529a3c6d6762
LaurelOlson <laurel.olson@outlook.com>
2016-03-01 22:09:48 +0000

    added admin column to users table

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,2 +1,31 @@
 class Admin::UsersController < ApplicationController
+
+  before_filter :restrict_access_admin
+
+  def index
+    @users = User.all
+  end
+
+  # def new
+  #   @admin = User.new
+  # end
+
+  # def create
+  #   @admin = User.create(user_params)
+
+  #   if @admin.save
+  #     session[:user_id] = @admin.id
+  #     redirect_to admin_users_path, notice "New admin #{@admin.full_name} successfully created"
+  #   else
+  #     render :admin_new
+  #   end
+
+  # end
+
+  # protected
+
+  # def user_params
+  #   params.require(:user).permit(:firstname, :lastname, :email, :password, :password_confirmation)
+  # end
+
 end
