26e5b3a1788aab334de5a04133dee3b18ed2958e
LaurelOlson <laurel.olson@outlook.com>
2016-03-03 22:00:13 +0000

    refactored

--- app/assets/javascripts/reviews.js
+++ app/assets/javascripts/reviews.js
@@ -1,18 +1,22 @@
 $(function() {
 
-  $("#new_review")
+  var new_review = $("#new_review");
 
-    .on("ajax:success", function(e, data, status, xhr) {
-    // add to reviews
-    $("#reviews").prepend('<div><p>' + data.review.text + '<p>' + data.review.rating_out_of_ten + '/10' + '<p><small>- ' + data.user )
-    })
+  new_review.on("ajax:success", function(e, data, status, xhr) {
+  // add to reviews
+  $("#reviews").prepend('<div><p>' + data.review.text + '<p>' + data.review.rating_out_of_ten + '/10' + '<p><small>- ' + data.user );
+  // clear form
+  $("#review_text").val('').focus();
+  $("#review_rating_out_of_ten").val('Rating');
 
-    .on("ajax:error", function(e, data, status, xhr) {
-      // add to errors
-      var errors = data.responseText.replace('[', '').replace(']', '').replace(/"/g, '').split(',')
-      errors.forEach(function(error) {
-        $("#review-errors").append('<li>' + error )
-      });
+  });
+
+  new_review.on("ajax:error", function(e, data, status, xhr) {
+    // add to errors
+    var errors = data.responseText.replace('[', '').replace(']', '').replace(/"/g, '').split(',');
+    errors.forEach(function(error) {
+      $("#review-errors").append('<li>' + error );
     });
+  });
 
 });
